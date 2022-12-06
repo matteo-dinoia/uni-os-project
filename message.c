2673576a8aa096e355f4774befbb886118c02cf7
Matteo <73781428+matteo-dinoia@users.noreply.github.com>
2022-12-06 15:50:27 +0100

    fixing bzero bug

--- message.c
+++ message.c
@@ -1,10 +1,10 @@
-#include <string.h>
+#include <strings.h>
 #include "message.h"
 
 struct commerce_msgbuf create_commerce_msgbuf(long sender, long receiver)
 {
 	struct commerce_msgbuf res;
-	bzero(res, sizeof(res));
+	bzero(&res, sizeof(res));
 
 	res.receiver = receiver;
 	res.sender = sender;
