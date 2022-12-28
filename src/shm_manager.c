31b571a90d48fd067076465ba8462f1f96be6882
Matteo <matteodinoia02@gmail.com>
2022-12-28 19:32:50 +0100

    adding bzero and removing 'useless' setter

--- src/shm_manager.c
+++ src/shm_manager.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include "header/shared_mem.h"
 #include "header/utils.h"
 #include "header/shm_manager.h"
@@ -50,6 +51,13 @@ void initialize_shm_manager(int permissions, const struct general *base_data)
 	_data_cargo = shmat(_id_cargo, NULL, 0);
 	_data_shop = shmat(_id_shop, NULL, 0);
 
+	if (base_data != NULL){
+		bzero(_data_port, sizeof(*_data_port) * SO_PORTI);
+		bzero(_data_ship, sizeof(*_data_ship) * SO_NAVI);
+		bzero(_data_cargo , sizeof(*_data_cargo) * SO_MERCI);
+		bzero(_data_shop, sizeof(*_data_shop) * SO_MERCI * SO_PORTI);
+	}
+
 }
 
 void close_shm_manager(){
@@ -93,44 +101,22 @@ double get_constants(int type_const)
 	}
 }
 
-/* COORD */
-struct coord get_coord_port(int id)
-{
-	/* COntrols */
-	return _data_port->coordinates;
-}
-void set_coord_port(int id, double x, double y)
-{
-	_data_port->coordinates.x = x;
-	_data_port->coordinates.x = y;
-}
-struct coord get_coord_ship(int id)
-{
-	return _data_ship->coordinates;
-}
-void set_coord_ship(int id, double x, double y){
-	_data_ship->coordinates.x = x;
-	_data_ship->coordinates.x = y;
-}
-
-/* DAY */
-int getday(){return _data->today;}
-void setday(int day){_data->today = day;}
-
 /* GETTER */
-/* PORT */
-int get_port_pid(int port_id){return _data_port[port_id].pid;}
+/* Day */
+int getday(){return _data->today;}
+/* Port */
+struct coord get_coord_port(int id){return _data_port->coordinates;}
 int get_port_daily_restock(int port_id){return _data_port[port_id].daily_restock_capacity;}
 int get_port_tot_dock(int port_id){return _data_port[port_id].dump_dock_tot;}
 bool_t had_port_swell(int port_id){return _data_port[port_id].dump_had_swell;}
-/* SHIP */
-int get_ship_pid(int ship_id){return _data_ship[ship_id].pid;}
+/* Ship */
+struct coord get_coord_ship(int id){return _data_ship->coordinates;}
 bool_t is_ship_moving(int ship_id){return _data_ship[ship_id].is_moving;}
 int get_ship_capacity(int ship_id){return _data_ship[ship_id].capacity;}
 bool_t is_ship_at_port(int ship_id){return _data_ship[ship_id].dump_is_at_dock;}
 bool_t had_ship_storm(int ship_id){return _data_ship[ship_id].dump_had_storm;}
 bool_t had_ship_maelstrom(int ship_id){return _data_ship[ship_id].dump_had_maelstrom;}
-/* CARGO */
+/* Cargo */
 int get_cargo_weight_batch(int cargo_id){return _data_cargo[cargo_id].weight_batch;}
 int get_cargo_shelf_life(int cargo_id){return _data_cargo[cargo_id].shelf_life;}
 int get_cargo_at_port(int cargo_id){return _data_cargo[cargo_id].dump_at_port;}
@@ -138,146 +124,19 @@ int get_cargo_in_ship(int cargo_id){return _data_cargo[cargo_id].dump_in_ship;}
 int get_cargo_expired_port(int cargo_id){return _data_cargo[cargo_id].dump_exipered_port;}
 int get_cargo_expired_ship(int cargo_id){return _data_cargo[cargo_id].dump_exipered_ship;}
 int get_cargo_tot_delivered(int cargo_id){return _data_cargo[cargo_id].dump_tot_delivered;}
-/* SHOP */
+/* Shop */
 int get_shop_quantity(int port_id, int cargo_id){return _data_shop[port_id].quantity;}
 int get_shop_tot_sent(int port_id, int cargo_id){return _data_shop[port_id].dump_tot_sent;}
 int get_shop_tot_received(int port_id, int cargo_id){return _data_shop[port_id].dump_tot_received;}
 
-
-
-
-
-int get_port(int port_id, int var_type)
+/* "SETTER" */
+void set_coord_port(int id, double x, double y)
 {
-	switch(var_type){
-		case VAR_PID: return _data_port[port_id].pid;
-		case VAR_DAILY_RESTOCK: return _data_port[port_id].daily_restock_capacity;
-		case VAR_DUMP_DOCK_TOT: return _data_port[port_id].dump_dock_tot;
-		case VAR_DUMP_HAD_SWELL: return _data_port[port_id].dump_had_swell;
-		default: return -1;
-	}
+	_data_port->coordinates.x = x;
+	_data_port->coordinates.x = y;
 }
-
-void set_port(int port_id, int var_type, int var)
-{
-	switch(var_type){
-		case VAR_PID:
-			_data_port[port_id].pid = var;
-			break;
-		case VAR_DAILY_RESTOCK:
-			_data_port[port_id].daily_restock_capacity = var;
-			break;
-		case VAR_DUMP_DOCK_TOT:
-			_data_port[port_id].dump_dock_tot = var;
-			break;
-		case VAR_DUMP_HAD_SWELL:
-			_data_port[port_id].dump_had_swell = var;
-			break;
-	}
+void set_coord_ship(int id, double x, double y){
+	_data_ship->coordinates.x = x;
+	_data_ship->coordinates.x = y;
 }
-
-/* SHIP */
-int get_ship(int ship_id, int var_type)
-{
-	switch(var_type){
-		case VAR_PID: return _data_ship[ship_id].pid;
-		case VAR_IS_MOVING: return _data_ship[ship_id].is_moving;
-		case VAR_CAPACITY: return _data_ship[ship_id].capacity;
-		case VAR_DUMP_IS_AT_DOCK: return _data_ship[ship_id].dump_is_at_dock;
-		case VAR_DUMP_HAD_STORM: return _data_ship[ship_id].dump_had_storm;
-		case VAR_DUMP_HAD_MAELSTROM: return _data_ship[ship_id].dump_had_maelstrom;
-		default: return -1;
-	}
-}
-
-void set_ship(int ship_id, int var_type, int var)
-{
-	switch(var_type){
-		case VAR_PID:
-			_data_ship[ship_id].pid = var;
-			break;
-		case VAR_IS_MOVING:
-			_data_ship[ship_id].is_moving = var;
-			break;
-		case VAR_CAPACITY:
-			_data_ship[ship_id].capacity = var;
-			break;
-		case VAR_DUMP_IS_AT_DOCK:
-			_data_ship[ship_id].dump_is_at_dock = var;
-			break;
-		case VAR_DUMP_HAD_STORM:
-			_data_ship[ship_id].dump_had_storm = var;
-			break;
-		case VAR_DUMP_HAD_MAELSTROM:
-			_data_ship[ship_id].dump_had_maelstrom = var;
-			break;
-	}
-}
-
-/* CARGO */
-int get_cargo(int cargo_id, int var_type)
-{
-	switch(var_type){
-		case VAR_WEIGHT_BATCH: return _data_cargo[cargo_id].weight_batch;
-		case VAR_SHELF_LIFE: return _data_cargo[cargo_id].shelf_life;
-		case VAR_DUMP_AT_PORT: return _data_cargo[cargo_id].dump_at_port;
-		case VAR_DUMP_IN_SHIP: return _data_cargo[cargo_id].dump_in_ship;
-		case VAR_DUMP_EXPIRED_PORT: return _data_cargo[cargo_id].dump_exipered_port;
-		case VAR_DUMP_EXPIRED_SHIP: return _data_cargo[cargo_id].dump_exipered_ship;
-		case VAR_DUMP_TOT_DELIVERED: return _data_cargo[cargo_id].dump_tot_delivered;
-		default: return -1;
-	}
-}
-
-void set_cargo(int cargo_id, int var_type, int var)
-{
-	switch(var_type){
-		case VAR_WEIGHT_BATCH:
-			_data_cargo[cargo_id].weight_batch = var;
-			break;
-		case VAR_SHELF_LIFE:
-			_data_cargo[cargo_id].shelf_life = var;
-			break;
-		case VAR_DUMP_AT_PORT:
-			_data_cargo[cargo_id].dump_at_port = var;
-			break;
-		case VAR_DUMP_IN_SHIP:
-			_data_cargo[cargo_id].dump_in_ship = var;
-			break;
-		case VAR_DUMP_EXPIRED_PORT:
-			_data_cargo[cargo_id].dump_exipered_port = var;
-			break;
-		case VAR_DUMP_EXPIRED_SHIP:
-			_data_cargo[cargo_id].dump_exipered_ship = var;
-			break;
-		case VAR_DUMP_TOT_DELIVERED:
-			_data_cargo[cargo_id].dump_tot_delivered = var;
-			break;
-	}
-}
-
-/* SHOP */
-int get_shop(int port_id, int cargo_id, int var_type)
-{
-	switch(var_type){
-		case VAR_QUANTITY: return _data_shop[port_id].quantity;
-		case VAR_DUMP_TOT_SENT: return _data_shop[port_id].dump_tot_sent;
-		case VAR_DUMP_TOT_RECEIVED: return _data_shop[port_id].dump_tot_received;
-		default: return -1;
-	}
-}
-
-void set_shop(int port_id, int cargo_id, int var_type, int var)
-{
-	switch(var_type){
-		case VAR_QUANTITY:
-			_data_shop[port_id].quantity = var;
-			break;
-		case VAR_DUMP_TOT_SENT:
-			_data_shop[port_id].dump_tot_sent = var;
-			break;
-		case VAR_DUMP_TOT_RECEIVED:
-			_data_shop[port_id].dump_tot_received = var;
-			break;
-	}
-}
+void setday(int day){_data->today = day;}
