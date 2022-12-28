1e29447be364844a88ea431cd54219d64c3d25d1
Matteo <matteodinoia02@gmail.com>
2022-12-28 19:35:14 +0100

    refactoring shm_manager.h

--- src/header/shm_manager.h
+++ src/header/shm_manager.h
@@ -6,40 +6,28 @@
 /* Prototype*/
 void initialize_constants();
 void close_all();
-/* Getter and setter */
 double get_constants(int index);
-int get_port(int port_id, int var_type);
-void set_port(int port_id, int var_type, int var);
-int get_ship(int ship_id, int var_type);
-void set_ship(int ship_id, int var_type, int var);
-int get_cargo(int cargo_id, int var_type);
-void set_cargo(int cargo_id, int var_type, int var);
-int get_shop(int port_id, int cargo_id, int var_type);
-void set_shop(int port_id, int cargo_id, int var_type, int var);
-/* Coord */
-struct coord get_coord_port(int id);
+/* Setter */
 void set_coord_port(int id, double x, double y);
-struct coord get_coord_ship(int id);
 void set_coord_ship(int id, double x, double y);
-/* Day */
-int get_day();
 void set_day(int day);
 
-
 /* GETTER */
-/* PORT */
-int get_port_pid(int port_id);
+/* Day */
+int get_day();
+/* Port */
+struct coord get_coord_port(int id);
 int get_port_daily_restock(int port_id);
 int get_port_tot_dock(int port_id);
 bool_t had_port_swell(int port_id);
-/* SHIP */
-int get_ship_pid(int ship_id);
+/* Ship */
+struct coord get_coord_ship(int id);
 bool_t is_ship_moving(int ship_id);
 int get_ship_capacity(int ship_id);
 bool_t is_ship_at_port(int ship_id);
 bool_t had_ship_storm(int ship_id);
 bool_t had_ship_maelstrom(int ship_id);
-/* CARGO */
+/* Cargo */
 int get_cargo_weight_batch(int cargo_id);
 int get_cargo_shelf_life(int cargo_id);
 int get_cargo_at_port(int cargo_id);
@@ -47,36 +35,10 @@ int get_cargo_in_ship(int cargo_id);
 int get_cargo_expired_port(int cargo_id);
 int get_cargo_expired_ship(int cargo_id);
 int get_cargo_tot_delivered(int cargo_id);
-/* SHOP */
+/* Shop */
 int get_shop_quantity(int port_id, int cargo_id);
 int get_shop_tot_sent(int port_id, int cargo_id);
 int get_shop_tot_received(int port_id, int cargo_id);
-
-
-/* Type *//*
-#define VAR_PID 0*/
-/* PORT *//*
-#define VAR_DAILY_RESTOCK 1
-#define VAR_DUMP_DOCK_TOT 2
-#define VAR_DUMP_HAD_SWELL 3*/
-/* SHIP *//*
-#define VAR_IS_MOVING 4
-#define VAR_CAPACITY 5
-#define VAR_DUMP_IS_AT_DOCK 6
-#define VAR_DUMP_HAD_STORM 7
-#define VAR_DUMP_HAD_MAELSTROM 8*/
-/* CARGO *//*
-#define VAR_WEIGHT_BATCH 9
-#define VAR_SHELF_LIFE 10
-#define VAR_DUMP_AT_PORT 11
-#define VAR_DUMP_IN_SHIP 12
-#define VAR_DUMP_EXPIRED_PORT 13
-#define VAR_DUMP_EXPIRED_SHIP 14
-#define VAR_DUMP_TOT_DELIVERED 15*/
-/* SHOP *//*
-#define VAR_QUANTITY 16
-#define VAR_DUMP_TOT_SENT 17
-#define VAR_DUMP_TOT_RECEIVED 18*/
 
 /* Simulation Constants */
 #define SO_LATO ((double)get_constants(0))
